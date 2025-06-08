da2bb681da6f3604572c13c13936d69264fcd2b5
beyondpie <szu@health.ucsd.edu>
2025-06-07 20:22:45 -0700

    get malloc workable

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,14 @@
+import scala.scalanative.unsafe._
+import scala.scalanative.unsafe.Size.intToSize
+
+@extern
+object libc {
+  def malloc(size: CSize): Ptr[Byte] = extern
+}
+
+
 object Main {
   def main(args: Array[String]): Unit =
-    println("Hello, world!")
+    val a = libc.malloc(10.toCSize)
+    println(a)
 }
